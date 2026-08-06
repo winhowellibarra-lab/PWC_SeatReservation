2b945dd0f2573311e8ff5738b53d336b85a9518d
winhowellibarra-lab <winhowell.ibarra@pwc.com>
2026-08-06 12:00:37 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
+  base: "/PWC_SeatReservation/",
 });
